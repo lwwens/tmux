eb1fe226c436d961751c1cff5eec3fa7d21876b2
Lwwen <lwwen1107@qq.com>
2018-12-02 17:41:25 +0800

    为mocp脚本添加注释。

--- tmux-mocp.sh
+++ tmux-mocp.sh
@@ -13,16 +13,23 @@
 # GNU Bash
 # -----------------------------------------
 
+# 判断mocp是否安装
 type mocp > /dev/null 2>&1 || exit 1
+# 判断mocp是否运行，若否，则shell返回码不为0
 mocp -Q %state > /dev/null 2>&1
 if [ $? = 0 ]; then
+    # 获得mocp正在播放的文件路径
     musicpath=`mocp -Q %file`
+    # 获得带有后缀的文件名
     musicfile=${musicpath##*/}
+    # 去掉文件后缀
     musicname=${musicfile%.*}
+    # 判断mocp是否正在播放
     if [ `mocp -Q %state` = 'PLAY' ]; then
         flag='♫';
     else
         flag='||';
     fi
+    # 返回mocp播放状态和歌曲名
     echo ${flag} ${musicname};
 fi
